7259a437c3d0b51af0cdf49b787cd4bea47db210
Christopher Swenson <swenson@radiobook.local>
2009-07-07 22:02:07 -0700

    Added basic auth support

--- rdb/web_server.py
+++ rdb/web_server.py
@@ -6,12 +6,11 @@ import rdb
 rdb.web_server.start_server()
 
 to start up a remote web server instance.
-
-TODO:
-  * Authentication support
 """
 
 import cherrypy
+# cherrypy 3.2+
+# import cherrypy.lib.auth_basic
 import jinja2
 import threading
 import sys
@@ -65,8 +64,39 @@ class RDBWebServer(object):
     return t.render(id=id, stack=stack, locals=locals, globals=globals)
 
 
-def start_server(port=8080, ssl=False, ssl_certificate=None, ssl_private_key=None):
+def start_server(port=8080,
+                 username=None,
+                 password=None,
+                 ssl=False, ssl_certificate=None, ssl_private_key=None):
+  
+  # set up the port
   cherrypy.config.update({'server.socket_port': port,})
+  
+  conf = {}               
+  # if a username / password is specified, then use it with basic authentication
+  if username and password:
+    userpassdict = {username: password}
+    
+    # save for CherryPy 3.2+
+    # checkpassword = cherrypy.lib.auth_basic.checkpassword_dict(userpassdict)
+    # basic_auth = {'tools.auth_basic.on': True,
+    #               'tools.auth_basic.realm': 'earth',
+    #               'tools.auth_basic.checkpassword': checkpassword,
+    # }
+    
+    # password pass-through
+    def pass_through(x): return x
+    
+    basic_auth = {'tools.basic_auth.on': True,
+                  'tools.basic_auth.realm': 'pdb',
+                  'tools.basic_auth.users': userpassdict,
+                  'tools.basic_auth.encrypt': pass_through,
+    }
+    conf = {'/': basic_auth}
+    #cherrypy.config.update({ '/' : basic_auth })
+    
+  
+  # configure SSL support, if asked
   if ssl:
     if ssl_certificate and os.path.exists(ssl_certificate) and \
        ssl_private_key and os.path.exists(ssl_private_key_file):
@@ -77,14 +107,40 @@ def start_server(port=8080, ssl=False, ssl_certificate=None, ssl_private_key=Non
     else:
       print("Bad SSL parameters.  Disabling SSL.")
       ssl = False
-      
-  _t = threading.Thread(name="RDBServerThread", target=cherrypy.quickstart, args=[RDBWebServer()])
+  
+  # finally, start the thread
+  _t = threading.Thread(name="RDBServerThread",
+                       target=cherrypy.quickstart,
+                       args=[RDBWebServer()],
+                       kwargs={'config': conf})
   _t.daemon = True
   _t.start()
+  return _t
 
 
 if __name__ == '__main__':
-  start_server()
+  import optparse
+  
+  parser = optparse.OptionParser()
+  parser.add_option("-p", "--port", dest="port", default=8080, type="int",
+                    help="username for authentication")
+  parser.add_option("-u", "--user", dest="username",
+                    help="username for authentication")
+  parser.add_option("-P", "--password", dest="password",
+                    help="password for authentication")
+  parser.add_option("-s", "--ssl", dest="ssl", action="store_true",
+                    help="Enable SSL")
+  parser.add_option("-c", "--cert", dest="ssl_certificate",
+                    help="SSL server certificate")
+  parser.add_option("-k", "--key", dest="ssl_private_key",
+                    help="SSL private key file")
+                    
+  (options, args) = parser.parse_args()
+  
+  
+  _t = start_server(port=options.port, username=options.username, password=options.password,
+                    ssl=options.ssl, ssl_certificate=options.ssl_certificate,
+                    ssl_private_key=options.ssl_private_key)
   
   def test_loop():
     """Simple loop to run in background so that the default thread view is interesting."""
